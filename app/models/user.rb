52c780d10bb7c49bd4531bdf1437b5671607a2b2
harryuan65 <harryuan.65@gmail.com>
2022-07-06 14:40:30 +0800

    doc: add comment to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+#
+# The User model to allow users to sign in.
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
