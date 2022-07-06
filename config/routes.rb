91957d25ca71e65b92cd7f2c5694c3e845d9b378
harryuan65 <harryuan.65@gmail.com>
2022-07-06 16:07:59 +0800

    style: lint to enforce double quote

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    confirmations: "users/confirmations",
+    passwords: "users/passwords",
+    registrations: "users/registrations",
+    sessions: "users/sessions"
     # omniauth_callbacks: 'users/omniauth_callbacks',
     # unlocks: 'users/unlocks'
   }
