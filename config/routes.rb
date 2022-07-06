28f5306c4f2d595d609a12649b6780da312b616a
harryuan65 <harryuan.65@gmail.com>
2022-07-06 15:40:18 +0800

    feat: update devise routes mapping, update form css
    forms in
    - registrations/new.html.erb
    - sessions/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
     sessions: 'users/sessions'
+    # omniauth_callbacks: 'users/omniauth_callbacks',
+    # unlocks: 'users/unlocks'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
