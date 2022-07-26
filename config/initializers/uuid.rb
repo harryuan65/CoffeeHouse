00a19be3a9704187e952ba6588cc6c6057208878
harryuan65 <harryuan.65@gmail.com>
2022-07-27 00:13:18 +0800

    feat: add uuid primary for generator

---
+++ config/initializers/uuid.rb
@@ -0,0 +1,5 @@
+# Add uuid support. Reference: https://pawelurbanek.com/uuid-order-rails
+# example: rails g model admin id:uuid name:string
+Rails.application.config.generators do |generator|
+  generator.orm :active_record, primary_key_type: :uuid
+end
