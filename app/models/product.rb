f04a9e4b5f5daa54e328ee088999d59ce5b4d89d
harryuan65 <harryuan.65@gmail.com>
2022-08-02 12:01:54 +0800

    feat: turbo stream for product crud

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,7 @@
 # Things for users to purchase
 #
 class Product < ApplicationRecord
+  after_create_commit { broadcast_append_to "products" }
+  after_update_commit { broadcast_replace_to "products" }
+  after_destroy_commit { broadcast_remove_to "products" }
 end
