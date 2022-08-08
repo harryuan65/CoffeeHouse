5db6be542941fadf2b36d6eb00b929e0808f1ed3
harryuan65 <harryuan.65@gmail.com>
2022-08-09 01:32:27 +0800

    feat: add error handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include ErrorHandler
+
   private
 
   def check_session
---
+++ app/controllers/concerns/error_handler.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+#
+# Catch and response to error for general cases
+#
+module ErrorHandler
+  extend ActiveSupport::Concern
+
+  def self.included(origin_class)
+    origin_class.class_eval do
+      rescue_from StandardError, with: :server_error_handler
+      rescue_from ActiveRecord::RecordInvalid, with: :record_invalid_handler
+      rescue_from ActionController::RoutingError, with: :route_not_found_handler
+      rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_handler
+    end
+  end
+
+  private
+
+  def record_not_found_handler(exception)
+    handle error_message: exception.to_s, status: :not_found
+  end
+
+  def record_invalid_handler(exception)
+    handle error_message: exception.to_s, status: :bad_request
+  end
+
+  def server_error_handler(exception)
+    error_details = "#{exception}\n#{exception.backtrace[0]}"
+    logger.fatal error_details
+    handle error_message: "#{exception}\n#{error_details}", status: :internal_server_error
+  end
+
+  def route_not_found_handler(exception)
+    handle error_message: exception.to_s, status: :not_found
+  end
+
+  def handle(error_message:, status:)
+    respond_to do |format|
+      format.turbo_stream do
+        flash.now[:alert] = error_message and render(partial: "shared/flash", status: status)
+      end
+      format.html do
+        flash[:alert] = error_message and redirect_to(root_path)
+      end
+    end
+  end
+end
