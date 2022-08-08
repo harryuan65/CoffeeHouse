cd10d87f84af4438f3a825ea1889b96a7983479a
harryuan65 <harryuan.65@gmail.com>
2022-08-08 23:33:05 +0800

    feat: add check session hook

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  def check_session
+    unless current_user
+      flash.now[:alert] = I18n.t("devise.failure.unauthenticated")
+      return render "users/sessions/request_session", status: 401
+    end
+  end
 end
