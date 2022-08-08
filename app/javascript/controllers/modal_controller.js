8d848c4338f0d3449e8f2e6bf6d5d12399aa612e
harryuan65 <harryuan.65@gmail.com>
2022-08-08 12:44:39 +0800

    chore: remove unused code

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,20 +1,8 @@
 import { Controller } from '@hotwired/stimulus'
-// import { Turbo } from '@hotwired/turbo-rails'
 
 // Connects to data-controller="modal"
 export default class extends Controller {
   static targets = ['modal']
-
-  connect () {
-    this.element.addEventListener('turbo:submit-end', this.next)
-  }
-
-  next (event) {
-    console.log(event.detail)
-    // if (event.detail.success) {
-    //   Turbo.visit(event.detail.fetchResponse.response.url)
-    // }
-  }
 
   closeModal () {
     this.element.remove()
