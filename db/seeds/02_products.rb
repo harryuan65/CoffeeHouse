ae73aff5ce1b1a9ac695a918b0c1cd1e3d9a21de
harryuan65 <harryuan.65@gmail.com>
2022-08-26 09:50:58 +0800

    chore: create stripe product when seeding

--- db/seeds/02_products.rb
+++ db/seeds/02_products.rb
@@ -1,3 +1,42 @@
+# Utility Functions
+
+# :reek:UtilityFunction
+# Get an excerpt from seed html content. Can get alphabet and zh_tw characters.
+#
+# @param [String] content: ActionText::Content.to_html
+#
+# @return [String]
+#
+def action_text_content_excerpt(content)
+  doc = Nokogiri::HTML(content)
+  doc.xpath("//div")[1].text.match(/(?:[^\n]+)|(?:[\u4E00-\u9FFC]+)\./).to_s
+end
+
+#
+# Create a stripe product for the product record, :reek:FeatureEnvy
+# @param [Product] product
+# @param [String] description: short desc for stripe product
+#
+def create_stripe_product(product, description)
+  return unless Stripe.api_key ||= ENV["STRIPE_API_KEY"]
+
+  stripe_product = Stripe::Product.create(
+    id: product.sku,
+    name: product.name,
+    description: description
+  )
+
+  Stripe::Price.create(
+    currency: "twd",
+    unit_amount: product.price.to_i * 100,
+    product: stripe_product["id"]
+  )
+
+  puts "Created stripe product: #{stripe_product["name"]}"
+end
+
+# Main Seeding Logic
+
 data_path = Rails.root.join("db/seeds/data/products.yml")
 products = Psych.safe_load(File.read(data_path), [Symbol])
 
@@ -7,4 +46,5 @@ products.each do |hash|
 
   product = Product.create!(hash)
   product.content.update!(body: ActionText::Content.new(content))
+  create_stripe_product(product, action_text_content_excerpt(content))
 end
