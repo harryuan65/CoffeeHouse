90efe47a2b4d4e016ca60ca9bb1d94461afe4175
harryuan65 <harryuan.65@gmail.com>
2022-12-02 11:10:58 +0800

    chore: auto gen password for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@gmail.com" }
     stripe_customer_id { "cus_xJzC2QfXWcEHeSGEqqvgUA" }
+    password { Devise.friendly_token.first(6) }
   end
 end
