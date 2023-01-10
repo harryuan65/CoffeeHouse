59cda35f8c44edd84b8a1301bd0a64c97cfaa1dc
harryuan65 <harryuan.65@gmail.com>
2023-01-10 15:22:01 +0800

    feat: add orders page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,17 @@
 # Handle order CRUD
 #
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   delegate :current_cart, to: :current_user
 
   def new
     result = MapCartToOrderItems.call(current_cart)
     @order_items = result.output
     respond_using(result)
+  end
+
+  def index
+    @orders = current_user.orders.all
+    @orders = Array.new(20, @orders.first)
   end
 end
